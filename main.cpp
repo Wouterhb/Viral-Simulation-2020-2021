1c461a6fabf8b2fc342698f118ad3cad308a601b
Wouter <wouterbeima@hotmail.com>
2021-05-30 17:56:08 +0200

    Added MovementStrategy selection logic to Main Updated Subject movement logic in Simulation

--- main.cpp
+++ main.cpp
@@ -15,47 +15,76 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "simulation.h"
+#include "html_canvas.h"
+#include "ChartJS_handler.h"
+#include "LockdownMovementStrategy.h"
+#include "RegularMovementStrategy.h"
 #include <iostream>
 #include <random>
 #include <math.h>
-#include "html_canvas.h"
-#include "ChartJS_handler.h"
+#include <memory>
 
-//Constants to control the simulation
+// Constants to control the simulation
 const int SUBJECT_COUNT = 200;
 const int SIM_WIDTH = 800;
 const int SIM_HEIGHT = 500;
 const int SUBJECT_RADIUS = 2;
+const float SOCIAL_DISTANCE_PERCENTAGE = 0.75; // Between 0 and 1
 
-int main() {
-    corsim::Simulation s(SIM_WIDTH,SIM_HEIGHT,std::make_unique<corsim::HTMLCanvas>(30,150,SIM_WIDTH,SIM_HEIGHT),
-        std::make_unique<corsim::ChartJSHandler>());
+// Initialize both strategies
+LockdownMovementStrategy lockdownMovementStrategy;
+RegularMovementStrategy regularMovementStrategy;
 
-    //Code to randomly generate certain numbers, which is done by using certain distributions
-    std::random_device rd;
-    std::mt19937 mt(rd());
+int main()
+{
+    corsim::Simulation simulation(SIM_WIDTH, SIM_HEIGHT, std::make_unique<corsim::HTMLCanvas>(30, 150, SIM_WIDTH, SIM_HEIGHT),
+                                  std::make_unique<corsim::ChartJSHandler>());
+
+    // Code to randomly generate certain numbers, which is done by using certain distributions
+    std::random_device randomDevice;
+    std::mt19937 mt(randomDevice());
     std::uniform_real_distribution<double> dist_w(1.0, SIM_WIDTH);
     std::uniform_real_distribution<double> dist_h(1.0, SIM_HEIGHT);
     std::uniform_real_distribution<double> dist_dx(-1.0, 1.0);
     std::uniform_real_distribution<double> dist_dy(-1.0, 1.0);
 
-    for (int i = 0; i<SUBJECT_COUNT; ++i)
+    for (int i = 0; i < SUBJECT_COUNT; ++i)
     {
-        double x = dist_w(mt); //Randomly generate x position
-        double y = dist_h(mt); //Randomly generate y position
-        
-        corsim::Subject su(x,y,SUBJECT_RADIUS,false);
+        // Randomly generate x position for the subject
+        double x = dist_w(mt);
+        // Randomly generate y position for the subject
+        double y = dist_h(mt);
 
-        su.set_dx(dist_dx(mt));
-        su.set_dy(dist_dy(mt));
+        // Create the subject
+        corsim::Subject subject(x, y, SUBJECT_RADIUS, false);
 
-        if(i == SUBJECT_COUNT-1)
+        // Set subject speed on x axis
+        subject.set_dx(dist_dx(mt));
+        // Set subject speed on y axis
+        subject.set_dy(dist_dy(mt));
+
+        // Set movement strategy of the subject
+        if (i < SUBJECT_COUNT * SOCIAL_DISTANCE_PERCENTAGE)
         {
-            su.infect();
+            // SOCIAL_DISTANCE_PERCENTAGE
+            subject.setMovementStrategy(&lockdownMovementStrategy);
+        }
+        else
+        {
+            // The rest
+            subject.setMovementStrategy(&regularMovementStrategy);
         }
 
-        s.add_subject(std::move(su));
-    }  
+        // Infect only the last subject
+        if (i == SUBJECT_COUNT - 1)
+        {
+            subject.infect();
+        }
 
-    s.run();
+        // Add subject to simulation
+        simulation.add_subject(std::move(subject));
+    }
+
+    // Run simulation
+    simulation.run();
 }
--- simulation.cpp
+++ simulation.cpp
@@ -15,191 +15,195 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "simulation.h"
-#include <iostream>
 #include <emscripten.h>
 #include <math.h>
 
 namespace corsim
 {
 
-Simulation::Simulation(int width, int height, std::unique_ptr<Canvas> canvas, std::unique_ptr<StatisticsHandler> sh) : 
-    _sim_width{width}, _sim_height{height}, _canvas{std::move(canvas)}, _sh{std::move(sh)} {}
+    Simulation::Simulation(int width, int height, std::unique_ptr<Canvas> canvas, std::unique_ptr<StatisticsHandler> statisticsHandler) : _sim_width{width}, _sim_height{height}, _canvas{std::move(canvas)}, _statisticsHandler{std::move(statisticsHandler)} {}
 
-void Simulation::add_subject(Subject&& s)
-{
-    this->_subjects.emplace_back(std::move(s));
-}
-
-void Simulation::run()
-{
-    if(running)
+    void Simulation::add_subject(Subject &&subject)
     {
-        return;
+        this->_subjects.emplace_back(std::move(subject));
     }
 
-    running = true;
-
-    while(true)
+    void Simulation::run()
     {
-        this->tick();
-        emscripten_sleep(tick_speed);
-    }
-}
-
-int counter  = 0;
-
-void Simulation::tick()
-{
-    counter++;
-
-    double dt = tick_speed / 10.0;
-
-    std::vector<Subject*> collision_checker;
-
-    for(Subject& s : _subjects)
-    {
-        collision_checker.emplace_back(&s);
-
-        wall_collision(s);
-    }
-
-    for(int i = collision_checker.size()-1; i < collision_checker.size(); i--)
-    {
-        Subject* current_checking = collision_checker.at(i);
-        collision_checker.erase(collision_checker.end());
-
-        for(Subject* s : collision_checker)
+        if (running)
         {
-            subject_collision(*current_checking, *s);
+            return;
+        }
+
+        running = true;
+
+        while (true)
+        {
+            this->tick();
+            emscripten_sleep(tick_speed);
         }
     }
 
-    int numberInfected = 0;
+    int counter = 0;
 
-    for(Subject& s : _subjects)
+    void Simulation::tick()
     {
-        s.set_x(s.x() + s.dx() * dt);
-        s.set_y(s.y() + s.dy() * dt);
+        counter++;
 
-        if(s.infected())
+        double dt = tick_speed / 10.0;
+
+        std::vector<Subject *> collision_checker;
+
+        for (Subject &subject : _subjects)
         {
-            numberInfected++;
+            collision_checker.emplace_back(&subject);
+
+            wall_collision(subject);
+        }
+
+        for (int i = collision_checker.size() - 1; i < collision_checker.size(); i--)
+        {
+            Subject *current_checking = collision_checker.at(i);
+            collision_checker.erase(collision_checker.end());
+
+            for (Subject *subject : collision_checker)
+            {
+                subject_collision(*current_checking, *subject);
+            }
+        }
+
+        int numberInfected = 0;
+
+        for (Subject &subject : _subjects)
+        {
+            subject.move(dt);
+
+            if (subject.infected())
+            {
+                numberInfected++;
+            }
+        }
+
+        if (counter % 30 == 0)
+        {
+            _statisticsHandler.get()->communicate_number_infected(counter / 30, numberInfected);
+        }
+
+        draw_to_canvas();
+    }
+
+    void Simulation::draw_to_canvas()
+    {
+        _canvas.get()->clear();
+        _canvas.get()->draw_rectangle(0, 0, 1, _sim_height, BLACK);
+        _canvas.get()->draw_rectangle(0, 0, _sim_width, 1, BLACK);
+        _canvas.get()->draw_rectangle(0, _sim_height - 1, _sim_width, 1, BLACK);
+        _canvas.get()->draw_rectangle(_sim_width - 1, 0, 1, _sim_height, BLACK);
+
+        for (Subject &subject : _subjects)
+        {
+            CanvasColor color = BLUE;
+
+            if (subject.infected())
+            {
+                color = RED;
+            }
+
+            _canvas.get()->draw_ellipse(subject.x(), subject.y(), subject.radius(), color);
         }
     }
 
-    if(counter % 30 == 0)
+    void Simulation::wall_collision(Subject &subject)
     {
-        _sh.get()->communicate_number_infected(counter/30,numberInfected);
-    }
-    
-
-    draw_to_canvas();
-}
-
-void Simulation::draw_to_canvas()
-{
-    _canvas.get()->clear();
-    _canvas.get()->draw_rectangle(0,0,1,_sim_height,BLACK);
-    _canvas.get()->draw_rectangle(0,0,_sim_width,1,BLACK);
-    _canvas.get()->draw_rectangle(0,_sim_height-1,_sim_width,1,BLACK);
-    _canvas.get()->draw_rectangle(_sim_width-1,0,1,_sim_height,BLACK);
-
-    for(Subject& s : _subjects)
-    {
-        CanvasColor c = BLUE;
-
-        if(s.infected())
+        if (subject.x() - subject.radius() + subject.dx() < 0 ||
+            subject.x() + subject.radius() + subject.dx() > _sim_width)
         {
-            c = RED;
+            subject.set_dx(subject.dx() * -1);
         }
-
-        _canvas.get()->draw_ellipse(s.x(), s.y(), s.radius(), c);
-    }
-}
-
-void Simulation::wall_collision(Subject& s)
-{
-    if (s.x() - s.radius() + s.dx() < 0 ||
-        s.x() + s.radius() + s.dx() > _sim_width) {
-        s.set_dx(s.dx() * -1);
-    }
-    if (s.y() - s.radius() + s.dy() < 0 ||
-        s.y() + s.radius() + s.dy() > _sim_height) {
-        s.set_dy(s.dy() * -1);
-    }
-    if (s.y() + s.radius() > _sim_height) {
-        s.set_y(_sim_height - s.radius());
-    }
-    if (s.y() - s.radius() < 0) {
-        s.set_y(s.radius());
-    }
-    if (s.x() + s.radius() > _sim_width) {
-        s.set_x(_sim_width - s.radius());
-    }
-    if (s.x() - s.radius() < 0) {
-        s.set_x(s.radius());
-    }
-}
-
-double distance(Subject& s1, Subject& s2)
-{
-    return sqrt(pow(s1.x() - s2.x(),2) + pow(s1.y() - s2.y(),2));
-}
-
-void Simulation::subject_collision(Subject& s1, Subject& s2)
-{
-    double dist = distance(s1, s2);
-
-    if(dist < s1.radius() + s2.radius())
-    {
-        if(s1.infected() || s2.infected())
+        if (subject.y() - subject.radius() + subject.dy() < 0 ||
+            subject.y() + subject.radius() + subject.dy() > _sim_height)
         {
-            s1.infect();
-            s2.infect();
-        }        
-
-        double theta1 = s1.angle();
-        double theta2 = s2.angle();
-        double phi = atan2(s1.x() - s2.x(), s1.y() - s2.y());
-
-        double dx1F = ((2.0*cos(theta2 - phi)) / 2) * cos(phi) + sin(theta1-phi) * cos(phi+M_PI/2.0);
-        double dy1F = ((2.0*cos(theta2 - phi)) / 2) * sin(phi) + sin(theta1-phi) * sin(phi+M_PI/2.0);
-        
-        double dx2F = ((2.0*cos(theta1 - phi)) / 2) * cos(phi) + sin(theta2-phi) * cos(phi+M_PI/2.0);
-        double dy2F = ((2.0*cos(theta1 - phi)) / 2) * sin(phi) + sin(theta2-phi) * sin(phi+M_PI/2.0);
-
-        s1.set_dx(dx1F);                
-        s1.set_dy(dy1F);                
-        s2.set_dx(dx2F);                
-        s2.set_dy(dy2F);
-
-        static_collision(s1, s2, false);
-    }
-}
-
-void Simulation::static_collision(Subject& s1, Subject& s2, bool emergency)
-{
-    double overlap = s1.radius() + s2.radius() - distance(s1, s2);
-    Subject& smallerObject = s1.radius() < s2.radius() ? s1 : s2;
-    Subject& biggerObject = s1.radius() > s2.radius() ? s1 : s2;
-
-    if(emergency)
-    {
-        Subject& temp = smallerObject;
-        smallerObject = biggerObject;
-        biggerObject = temp;
-    }
-
-    double theta = atan2((biggerObject.y() - smallerObject.y()), (biggerObject.x() - smallerObject.x()));
-    smallerObject.set_x(smallerObject.x() - overlap * cos(theta));
-    smallerObject.set_y(smallerObject.y() - overlap * sin(theta));
-
-    if (distance(s1, s2) < s1.radius() + s2.radius()) {
-        if (!emergency)
+            subject.set_dy(subject.dy() * -1);
+        }
+        if (subject.y() + subject.radius() > _sim_height)
         {
-            static_collision(s1, s2, true);
+            subject.set_y(_sim_height - subject.radius());
+        }
+        if (subject.y() - subject.radius() < 0)
+        {
+            subject.set_y(subject.radius());
+        }
+        if (subject.x() + subject.radius() > _sim_width)
+        {
+            subject.set_x(_sim_width - subject.radius());
+        }
+        if (subject.x() - subject.radius() < 0)
+        {
+            subject.set_x(subject.radius());
         }
     }
-}
+
+    double distance(Subject &subject1, Subject &subject2)
+    {
+        return sqrt(pow(subject1.x() - subject2.x(), 2) + pow(subject1.y() - subject2.y(), 2));
+    }
+
+    void Simulation::subject_collision(Subject &subject1, Subject &subject2)
+    {
+        double dist = distance(subject1, subject2);
+
+        if (dist < subject1.radius() + subject2.radius())
+        {
+            if (subject1.infected() || subject2.infected())
+            {
+                subject1.infect();
+                subject2.infect();
+            }
+
+            double theta1 = subject1.angle();
+            double theta2 = subject2.angle();
+            double phi = atan2(subject1.x() - subject2.x(), subject1.y() - subject2.y());
+
+            double dx1F = ((2.0 * cos(theta2 - phi)) / 2) * cos(phi) + sin(theta1 - phi) * cos(phi + M_PI / 2.0);
+            double dy1F = ((2.0 * cos(theta2 - phi)) / 2) * sin(phi) + sin(theta1 - phi) * sin(phi + M_PI / 2.0);
+
+            double dx2F = ((2.0 * cos(theta1 - phi)) / 2) * cos(phi) + sin(theta2 - phi) * cos(phi + M_PI / 2.0);
+            double dy2F = ((2.0 * cos(theta1 - phi)) / 2) * sin(phi) + sin(theta2 - phi) * sin(phi + M_PI / 2.0);
+
+            subject1.set_dx(dx1F);
+            subject1.set_dy(dy1F);
+            subject2.set_dx(dx2F);
+            subject2.set_dy(dy2F);
+
+            static_collision(subject1, subject2, false);
+        }
+    }
+
+    void Simulation::static_collision(Subject &subject1, Subject &subject2, bool emergency)
+    {
+        double overlap = subject1.radius() + subject2.radius() - distance(subject1, subject2);
+        Subject &smallerObject = subject1.radius() < subject2.radius() ? subject1 : subject2;
+        Subject &biggerObject = subject1.radius() > subject2.radius() ? subject1 : subject2;
+
+        if (emergency)
+        {
+            Subject &temp = smallerObject;
+            smallerObject = biggerObject;
+            biggerObject = temp;
+        }
+
+        double theta = atan2((biggerObject.y() - smallerObject.y()), (biggerObject.x() - smallerObject.x()));
+
+        smallerObject.set_x(smallerObject.x() - overlap * cos(theta));
+        smallerObject.set_y(smallerObject.y() - overlap * sin(theta));
+
+        if (distance(subject1, subject2) < subject1.radius() + subject2.radius())
+        {
+            if (!emergency)
+            {
+                static_collision(subject1, subject2, true);
+            }
+        }
+    }
 
 }
